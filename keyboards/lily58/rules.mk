b7c79bb24acae1f1240c03c39f0f8459f1635529
Shrey Somaiya <shrey.somaiya@gmail.com>
2022-07-09 17:27:16 +1000

    oh no Poo that's not honey, that's breaking API changes

--- keyboards/lily58/rules.mk
+++ keyboards/lily58/rules.mk
@@ -19,5 +19,14 @@ RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
 OLED_ENABLE = yes
 OLED_DRIVER = SSD1306    # OLED display
 SPLIT_KEYBOARD = yes
-
+OLED_DRIVER_ENABLE= yes     # OLED display
 DEFAULT_FOLDER = lily58/rev1
+
+# If you want to change the display of OLED, you need to change here
+SRC +=  ./lib/rgb_state_reader.c \
+        ./lib/layer_state_reader.c \
+        ./lib/logo_reader.c \
+        ./lib/keylogger.c \
+        # ./lib/mode_icon_reader.c \
+        # ./lib/host_led_state_reader.c \
+        # ./lib/timelogger.c \
